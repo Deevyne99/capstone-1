585467e0a2c9d6bea13ab264e45e947e8f93d494
unknown <kaludivine545@gmail.com>
2023-08-15 04:00:20 +0100

    adding components gradually from start

---
+++ src/components/directory/DirectoryComponent.js
@@ -0,0 +1,12 @@
+import CategoryItem from '../category-component/CategoryItem'
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />
+      })}
+    </div>
+  )
+}
+
+export default Directory
